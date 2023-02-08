fb580eb12c68311495b495aee6b2cea36fe0eb49
yuanlee <yuanlee@163.com>
2023-02-08 14:17:40 +0800

    Forked from: Vue3-TS-Template@v1.0.4

---
+++ src/layout/Content/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <RouterView v-slot="{ Component, route }">
+    <transition name="fade-slide" mode="out-in">
+      <div class="container">
+        <component :is="Component" :key="route.path" />
+      </div>
+    </transition>
+  </RouterView>
+</template>
---
+++ src/layout/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="layout">
+    <LayoutSideBar class="sideBar" />
+
+    <main>
+      <LayoutHeader />
+      <LayoutContent />
+    </main>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import LayoutSideBar from "./SideBar/index.vue";
+import LayoutHeader from "./Header/index.vue";
+import LayoutContent from "./Content/index.vue";
+</script>
+
+<style scoped lang="less">
+@sideWidth: 210px;
+
+.layout {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100%;
+  .sideBar {
+    position: fixed;
+    width: @sideWidth;
+    height: 100vh;
+    background-color: #001529;
+  }
+  main {
+    margin-left: @sideWidth;
+    .container {
+      padding: 10px;
+    }
+  }
+}
+</style>
